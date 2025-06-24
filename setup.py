6bf863877dd2e23f3c0b85f0aeeaa00b21aa702b
ahoop004 <ahoop004@odu.edu>
2025-06-24 12:10:33 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='f110_gym',
+      version='0.2.1',
+      author='Hongrui Zheng',
+      author_email='billyzheng.bz@gmail.com',
+      url='https://f1tenth.org',
+
+      package_dir={'': 'f110_gymnasium'},
+      install_requires=['gymnasium',
+		        'numpy',
+                        'Pillow',
+                        'scipy',
+                        'numba',
+                        'pyyaml',
+                        'pyglet',
+                        # 'pyopengl',
+                        ]
+      )
